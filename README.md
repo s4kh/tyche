cb713c64e7d61739b1c53f0451302a5049a97bfc
s7kh <syerikjan@nomadays.com>
2019-09-27 11:58:07 +0800

    First draft - Single worker

---
+++ README.md
@@ -0,0 +1,4 @@
+## Approach
+1. Clear out misunderstandings in the challenge. Questions about the task.
+2. Single process prototype. Spawn single worker and send get request
+3. Carry on
---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	// ./worker -workerId 1 -port 3001
+	// go func() {
+	cmd := exec.Command("bin/worker.linux", "-workerId", "1", "-port", "3001")
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("cmd.Start() failed with %s: %s\n", err, stderr.String())
+	}
+	// }()
+
+	// Have to wait?
+	time.Sleep(1 * time.Second)
+	// pr, pw := io.Pipe()
+	//`http://localhost:3001/rnd?n=100`
+	res, err := http.Get("http://localhost:3001/rnd?n=40")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+}
