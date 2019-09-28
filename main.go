e35a805f9a29ef48b1da299e3ee82a2a66515b77
s4kh <kh.syerikjan@gmail.com>
2019-09-28 17:44:53 +0800

    Using 16 goroutines to consume numbers

--- main.go
+++ main.go
@@ -8,21 +8,13 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
+	"sync"
 	"time"
 )
 
-func main() {
-	// ./worker -workerId 1 -port 3001
-	/*
-		TODOs:
-		1. Consume numbers count them using new line
-		2. Spawn multiple workers with different id and port
-		 a. How to know which workers are ready to accept request
-		3. Send requests to workers - multiple clients also
-	*/
-	// go func() {
-	start := time.Now()
-	cmd := exec.Command("bin/worker.linux", "-workerId", "1", "-port", "3001")
+func spawnWorker(workerID string, port string) {
+	cmd := exec.Command("bin/worker.linux", "-workerId", workerID, "-port", port)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	cmd.Stdout = &out
@@ -31,18 +23,43 @@ func main() {
 	if err := cmd.Start(); err != nil {
 		log.Fatalf("cmd.Start() failed with %s: %s\n", err, stderr.String())
 	}
-	// }()
+	fmt.Printf("Spawned %s on :%s\n", workerID, port)
+}
 
-	// Have to wait?
-	time.Sleep(1 * time.Second)
-	// pr, pw := io.Pipe()
-	//`http://localhost:3001/rnd?n=100`
-	res, err := http.Get("http://localhost:3001/rnd?n=40")
+func callEndpoint(wg *sync.WaitGroup, endpoint string) {
+	defer wg.Done()
+
+	res, err := http.Get(endpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer res.Body.Close()
 	io.Copy(os.Stdout, res.Body)
+	fmt.Println("consumed ", endpoint)
+}
+
+func main() {
+	/*
+		TODOs:
+		1. Consume numbers count them using new line
+		2. Spawn multiple workers with different id and port
+		 a. How to know which workers are ready to accept request
+		3. Send requests to workers - multiple clients also
+	*/
+	start := time.Now()
+	var wg sync.WaitGroup
+
+	for i := 1; i < 17; i++ {
+		workerID := strconv.Itoa(i)
+		port := strconv.Itoa(3000 + i)
+		spawnWorker(workerID, port)
+		// Have to wait?
+		time.Sleep(time.Second)
+		endpoint := "http://localhost:" + port + "/rnd?n=100"
+		wg.Add(1)
+		go callEndpoint(&wg, endpoint)
+	}
+	wg.Wait()
 	elapsed := time.Since(start)
-	fmt.Println(elapsed)
+	fmt.Println("Time elapsed:", elapsed)
 }
